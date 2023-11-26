829651dae08a26721cc6275c32e0f02817af9cfa
AdamJ335 <76915670+AdamJ335@users.noreply.github.com>
2023-11-26 12:30:33 +0000

    [fix issue 4] basic tic-tac-toe-game (#5)

--- game.py
+++ game.py
@@ -1,78 +1,24 @@
 import pygame
 import logging
-import pyautogui
 
 from board import Board
 from const import WIDTH
 from const import HEIGHT
-from const import BG_COLOUR
+from const import CROSS_COLOUR
+from const import CIRCLE_COLOUR
 
 class Game:
 
-    def __init__(self, ultimate=False, max=False, singlePlayer=False):
+    def __init__(self, ultimate=False, max=False):
         self.ultimate = ultimate
         self.max = max
-        self.singlePlayer = singlePlayer
         self.board = Board(ultimate=ultimate, max=max)
         self.player = 1
         self.playing = True
-        self.next_cell = [-1,-1]
         pygame.font.init()
 
-    def play_game(self, screen):
-        pygame.display.set_caption('ULTIMATE TIC TAC TWIST')
-        
-        self.board.render(screen)
-        logging.info('Starting in game loop')
-        self.board.highlight_valid_move(screen, self.next_cell, self.player)
-        while True:
-            
-            for event in pygame.event.get():
-
-                # click
-                if event.type == pygame.MOUSEBUTTONDOWN and self.playing:
-                    xclick, yclick = event.pos
-
-                    if self.board.valid_sqr(xclick, yclick, self.next_cell, self.max):
-                        # next_board.fill(BG_COLOUR)
-                        self.next_cell = self.board.mark_sqr(xclick, yclick, self.player, self.next_cell)
-                        logging.info('nextCell = %s',self.next_cell)
-                        self.board.draw_fig(screen, xclick, yclick)
-
-                        self.nextCellRow = self.next_cell[0]
-                        self.nextCellCol = self.next_cell[1]
-                        if self.board.next_board_full(xclick, yclick, self.next_cell, self.ultimate, self.max):
-                            self.next_cell = [-1,-1] 
-                        # ultimate winner ?
-                        winner = self.board.check_draw_win(screen)
-                        if winner:
-                            self.board.manage_win(screen, winner, onmain=True)
-                            self.ultimate_winner(screen, winner)
-
-                        self.next_turn()
-                        self.board.highlight_valid_move(screen, self.next_cell, self.player)
-                    else:
-                        logging.info('Invalid move!')
-                        pyautogui.alert("Your move does not work as it is Invalid!")
-
-                # keypress
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_r:
-                        logging.info('Restarting the game!')
-                        self.restart()
-                        screen.fill(BG_COLOUR)
-                        self.board.render(screen)
-                        self.next_cell = [-1,-1]
-                    if event.key == pygame.K_ESCAPE:
-                        logging.info('Returning to Main menu')
-                        screen.fill(BG_COLOUR)
-                        return
-                # quit
-                if event.type == pygame.QUIT:
-                    logging.info('Quitting to Menu')
-                    screen.fill(BG_COLOUR)
-                    return
-            pygame.display.update()
+    def render_board(self, surface):
+        self.board.render(surface)
 
     def next_turn(self):
         self.player = 2 if self.player == 1 else 1
@@ -81,7 +27,7 @@ class Game:
         print('ULTIMATE WINNER! ->', winner)
 
         if winner == 1:
-            # color = CROSS_COLOUR
+            color = CROSS_COLOUR
             # desc
             iDesc = (WIDTH // 2 - 110, HEIGHT // 2 - 110)
             fDesc = (WIDTH // 2 + 110, HEIGHT // 2 + 110)
@@ -89,20 +35,23 @@ class Game:
             iAsc = (WIDTH // 2 - 110, HEIGHT // 2 + 110)
             fAsc = (WIDTH // 2 + 110, HEIGHT // 2 - 110)
             # draw
-            pygame.draw.line(surface, BG_COLOUR, iDesc, fDesc, 22)
-            pygame.draw.line(surface, BG_COLOUR, iAsc, fAsc, 22)
+            pygame.draw.line(surface, color, iDesc, fDesc, 22)
+            pygame.draw.line(surface, color, iAsc, fAsc, 22)
 
         else:
-            # color = CIRCLE_COLOUR
+            color = CIRCLE_COLOUR
             # center
             center = (WIDTH // 2, HEIGHT // 2   )
-            pygame.draw.circle(surface, BG_COLOUR, center, WIDTH // 4, 22)
+            pygame.draw.circle(surface, color, center, WIDTH // 4, 22)
         
         font = pygame.font.SysFont('monospace', 64)
-        lbl = font.render('WINNER!', 1, BG_COLOUR)
+        lbl = font.render('ULTIMATE WINNER!', 1, color)
         surface.blit(lbl, (WIDTH // 2 - lbl.get_rect().width // 2, HEIGHT // 2 + 220))
 
         self.playing = False
+
+    def check_next_board_full(self, xclick, yclick, nextCell):
+        return self.board.next_board_full(xclick, yclick, nextCell, self.ultimate)
     
     def restart(self):
         self.__init__(self.ultimate, self.max)
